c37ee5ba36f4a0bd37a9e701b44785e46d558e9f
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-07-04 14:47:19 +0700

    update default handlers

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,13 +1,12 @@
-import {StarterEntity} from "../types";
+import { StarterEntity } from "../types";
 
 export async function handleBlock(block: any): Promise<void> {
-
+  const newEntity = new StarterEntity(block.round);
+  newEntity.field2 = "block";
+  await newEntity.save();
 }
-export async function handleTransaction(txn:any):Promise<void>{
-    
-    
+export async function handleTransaction(txn: any): Promise<void> {
+  const newEntity = new StarterEntity(txn.id);
+  newEntity.field2 = "txn";
+  await newEntity.save();
 }
-
-
-
-
