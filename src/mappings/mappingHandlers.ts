68856c685b30d1b61b02fc21502d63832d49abed
nguyentuan7277 <tuan97.gs19@gmail.com>
2022-07-12 11:02:01 +0700

    change info

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,12 +1,15 @@
 import { StarterEntity } from "../types";
+import { AlgorandBlock, AlgorandTransaction } from "@subql/types";
 
-export async function handleBlock(block: any): Promise<void> {
-  const newEntity = new StarterEntity(block.round);
+export async function handleBlock(block: AlgorandBlock): Promise<void> {
+  const newEntity = new StarterEntity(block.round.toString());
+  newEntity.field1 = block.round;
   newEntity.field2 = "block";
   await newEntity.save();
 }
-export async function handleTransaction(txn: any): Promise<void> {
+export async function handleTransaction(txn: AlgorandTransaction): Promise<void> {
   const newEntity = new StarterEntity(txn.id);
+  newEntity.field1 = txn.roundTime;
   newEntity.field2 = "txn";
   await newEntity.save();
 }
